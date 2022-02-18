00db17c1af839387d405b850fdb42fff6f19a2ca
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2022-02-18 13:50:40 -0300

    remove options

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,4 +45,4 @@ fi
 
 cd /github/workspace/
 
-clojure -Sdeps "{:deps {io.github.clj-holmes/clj-watson {:git/tag \"${clj_watson_tag}\" :git/sha \"${clj_watson_sha}\"}}}" -M -m clj-watson.cli ${output_cmd}
+clojure -Sdeps "{:deps {io.github.clj-holmes/clj-watson {:git/tag \"${clj_watson_tag}\" :git/sha \"${clj_watson_sha}\"}}}" -M -m clj-watson.cli scan -p deps.edn #_${output_cmd}

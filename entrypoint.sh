a941d28fd23933a80756a7da700124b8726b55f5
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2022-02-18 15:09:53 -0300

    please-work

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ output_type="$6"
 fail_on_result="$7"
 suggestion_fix="$8"
 database_strategy="$9"
-output_cmd="-M -m clj-watson.cli scan -p ${deps_edn_path}"
+output_cmd="clojure -Sdeps \"{:deps {io.github.clj-holmes/clj-watson {:git/tag \\\"${clj_watson_tag}\\\" :git/sha \\\"${clj_watson_sha}\\\"}}}\" -M -m clj-watson.cli scan -p ${deps_edn_path}"
 
 if [[ ! -z $aliases ]]; then
   IFS=','
@@ -44,4 +44,4 @@ fi
 
 cd /github/workspace/
 
-clojure -Sdeps "{:deps {io.github.clj-holmes/clj-watson {:git/tag \"${clj_watson_tag}\" :git/sha \"${clj_watson_sha}\"}}}" ${output_cmd[@]}
+bash -c "${output_cmd}"

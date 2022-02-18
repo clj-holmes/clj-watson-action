e046e1a70403f78a423bf6a6173324ceada40b02
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2022-02-18 15:17:50 -0300

    add aliases and database strategy support (#1)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-deps_edn_path="$1"
-dependency_check_properties="$2"
-output_type="$3"
-fail_on_result="$4"
-suggestion_fix="$5"
+clj_watson_tag="$1"
+clj_watson_sha="$2"
+deps_edn_path="$3"
+aliases="$4"
+dependency_check_properties="$5"
+output_type="$6"
+fail_on_result="$7"
+suggestion_fix="$8"
+database_strategy="$9"
+output_cmd="clojure -Sdeps \"{:deps {io.github.clj-holmes/clj-watson {:git/tag \\\"${clj_watson_tag}\\\" :git/sha \\\"${clj_watson_sha}\\\"}}}\" -M -m clj-watson.cli scan -p ${deps_edn_path}"
 
-clj_watson_tag="v2.1.3"
-clj_watson_sha="19636f2"
-
-output_cmd="scan -p ${deps_edn_path}"
+if [[ ! -z $aliases ]]; then
+  IFS=','
+  read -a strarr <<< "$aliases"
+  for alias in "${strarr[@]}";
+  do
+    output_cmd="${output_cmd} -a ${alias}"
+  done
+fi
 
 if [[ $fail_on_result == "true" ]]; then
-  fail_on_result_cmd="--fail-on-result";
+  output_cmd="${output_cmd} --fail-on-result";
 else
-  fail_on_result_cmd="--no-fail-on-result";
+  output_cmd="${output_cmd} --no-fail-on-result";
 fi
 
 if [[ ! -z $dependency_check_properties ]]; then
@@ -29,5 +38,10 @@ if [[ $suggestion_fix  == "true" ]]; then
   output_cmd="${output_cmd} -s";
 fi
 
+if [[ ! -z $database_strategy ]]; then
+  output_cmd="${output_cmd} -t ${database_strategy}";
+fi
+
 cd /github/workspace/
-clojure -Sdeps "{:deps {io.github.clj-holmes/clj-watson {:git/tag \"${clj_watson_tag}\" :git/sha \"${clj_watson_sha}\"}}}" -M -m clj-watson.cli $output_cmd
+
+bash -c "${output_cmd}"
